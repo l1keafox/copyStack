a00a4e0bd48ecba91bc76e1af5ba22fdbc65692e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-26 16:42:06 -0600

    userseed

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -8,27 +8,9 @@ db.once("open", async () => {
 	try {
 		await User.deleteMany({});
 		await User.create(userSeeds);
-		await Channel.deleteMany({});
-		await Channel.create(chatSeeds);
-		await GameCard.deleteMany({});
-		await GameCard.create(gameCardSeeds);
 
 		let users = await User.find();
-		let channels = await Channel.find();
-		let gameCards = await GameCard.find();
-		console.log(channels);
 		console.log(users);
-		console.log(gameCards);
-		for (const user of users) {
-			for (const channel of channels) {
-				const task = await Channel.findOneAndUpdate(
-					{ _id: channel._id },
-					{ $addToSet: { participants: { _id: user._id } } },
-					{ runValidators: true, new: true }
-				);
-			}
-		}
-
 		console.log("all done!");
 		process.exit(0);
 	} catch (err) {
